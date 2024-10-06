875f9ac50203ddff04f4f4a13950d314a0209f30
Yajat Gupta <142727035+yajatgupta24@users.noreply.github.com>
2024-10-06 23:26:53 +0530

    Update PACEApplication.jsx

--- src/components/PACEApplication/PACEApplication.jsx
+++ src/components/PACEApplication/PACEApplication.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import satelitImg from "../../assets/application.gif";
+import satelitImg from "../../assets/Application.gif";
 
 const Rapidscat = () => {
   return (
